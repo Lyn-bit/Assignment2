313ead7cdd567453746989c02d5f14eb8b75ec7e
lorei.stojna@studenti.unipd.it <lorei.stojna99@gmail.com>
2020-12-31 15:06:58 +0100

    modifica header

--- station.h
+++ station.h
@@ -2,19 +2,19 @@
 #define station_h
 
 #include <string>
+#include <vector>
 
 #include "train.h"
 
 class Station {
-	Station() = delete; //Forse
-	virtual std::string SendMsg(Train t, int time) = 0;
+	virtual std::string SendMsg(const Train& t, int time) = 0;
 	virtual std::string GetName() const = 0;
-	virtual std::string GetType() const = 0;
-	virtual std::string GetDistnace() const = 0;
-	virtual std::string GetTrainsAhead() const = 0;
+	virtual int GetType() const = 0;
+	virtual int GetDistnace() const = 0;
+	virtual std::vector<Train> GetTrainsAhead() const = 0;
 
-	virtual void PrintDepartureTime(Train t, int time) const = 0;
-	virtual void PrintArrivalTime(Train t, int time) const = 0;
+	virtual void PrintDepartureTime(const Train& t, int time) const = 0;
+	virtual void PrintArrivalTime(const Train& t, int time) const = 0;
 };
 
 #endif // !station_h
